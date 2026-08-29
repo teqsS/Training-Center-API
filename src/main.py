c7b28516900b25b17f82786841fd270259025592
yungwave <nikget@yandex.ru>
2026-08-29 18:27:19 +0300

    Introduce FastAPI application factory

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-import asyncio
+from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
 
-import uvicorn
 from fastapi import FastAPI
 
+from app.database import async_engine
 from app.routers import (
     courses_router,
     enrollments_router,
@@ -11,23 +12,28 @@ from app.routers import (
     system_router,
 )
 
-app = FastAPI(
-    title="training_center_api",
-    version="0.1.0",
-)
-app.include_router(system_router, prefix="/training_center_api")
-app.include_router(courses_router, prefix="/training_center_api")
-app.include_router(students_router, prefix="/training_center_api")
-app.include_router(enrollments_router, prefix="/training_center_api")
-app.include_router(statistics_router, prefix="/training_center_api")
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI) -> AsyncIterator[None]:
+    yield
+    await async_engine.dispose()
 
 
-async def main():
-    uvicorn.run(
-        app="main:app",
-        reload=True,
+def create_app() -> FastAPI:
+
+    app = FastAPI(
+        lifespan=lifespan,
+        title="training_center_api",
+        version="0.1.0",
     )
 
+    app.include_router(system_router, prefix="/training_center_api")
+    app.include_router(courses_router, prefix="/training_center_api")
+    app.include_router(students_router, prefix="/training_center_api")
+    app.include_router(enrollments_router, prefix="/training_center_api")
+    app.include_router(statistics_router, prefix="/training_center_api")
 
-if __name__ == "__main__":
-    asyncio.run(main())
+    return app
+
+
+app = create_app()
