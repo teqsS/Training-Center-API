c5a46aedd2b3a5f884f514dc489f44b853a41d8c
yungwave <nikget@yandex.ru>
2026-08-29 17:41:14 +0300

    Configure typed application settings

--- src/app/config.py
+++ src/app/config.py
@@ -9,9 +9,15 @@ class Settings(BaseSettings):
     DB_NAME: str
 
     @property
-    def DATABASE_URL_asyncpg(self):
+    def DATABASE_URL_asyncpg(self) -> str:
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=True,
+        extra="forbid",
+    )
+
 
 settings = Settings()
